b879b402b3334a112d446853773fd3e041863751
dabb3 <lcsc2001@outlook.com>
2023-09-06 01:16:46 -0300

    commit for first version of nest-api-challenge

---
+++ README.md
@@ -0,0 +1,12 @@
+To run the tests, in any environment you are, run:
+
+``` bash
+npm run test
+```
+
+*npm run test:cov* will give you the coverage of unit tests. for this project, the only missing one is the *users.service.ts* file.
+
+## import to remember
+to start database open a prompt and type mongosh database(on my case example: mongosh nest)
+after installing RabbitMQ, search for RabbitMQService - start on windows search bar to start RabbitMQ
+the same process can be done by RabbitMQService - stop to stop RabbitMQ
---
+++ src/validate/validate.filter.ts
@@ -0,0 +1,15 @@
+import { Catch, ExceptionFilter, ArgumentsHost, BadRequestException } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(BadRequestException)
+export class ValidationFilter implements ExceptionFilter {
+  catch(exception: BadRequestException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const errors = exception.getResponse();
+    response.status(400).json({
+      statusCode: 400,
+      errors: Array.isArray(errors) ? errors : [errors],
+    });
+  }
+}
